c0932dced1757169366005d7f6e7115cb1f8d82f
Brian Morrison II <brian@brianmorrison.me>
2022-11-03 01:53:01 -0500

    remove dep

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,10 @@
 module.exports = {
   content: [
     './src/**/*.{html,js,svelte,ts}',
-    './node_modules/tw-elements/dist/js/**/*.js'
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('tw-elements/dist/plugin')
   ],
 }
